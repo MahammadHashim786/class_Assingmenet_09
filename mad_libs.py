cd12015d561f051dc75f0cc441739df701841213
MahammadHashim786 <161362132+MahammadHashim786@users.noreply.github.com>
2025-04-10 08:21:29 +0500

    Madlibs

---
+++ mad_libs.py
@@ -0,0 +1,11 @@
+# Mad Libs
+
+name = input("Enter a name: ")
+father_name = input("Enter a father name: ")
+frt_day = input("Favourit day: ")
+frt_food = input("favourit food: ")
+
+
+madlibs = f"\nMy name is {name} My Father name {father_name} My favourit day {frt_day} My favourit food {frt_food} "
+
+print(f"Mad Libs Story\n{madlibs}")
